2b488f874aeec5eb1e429b1221af9ea2d73e856c
Adrian <vadriancastillo2@gmail.com>
2024-02-27 08:05:43 -0600

    Added some code for header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Search from "./Search.svelte";
+    import Nav from "./Nav.svelte";
+</script>
+
+<div class="border-b">
+    <div class="flex h-16 items-center px-4">
+        <h2 class="text-lg font-semibold">Playground</h2>
+        <Nav class="mx-6" />
+        <div class="ml-auto flex items-center space-x-4">
+            <Search />
+        </div>
+    </div>
+</div>
